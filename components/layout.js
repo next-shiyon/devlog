6ab817d592dde142f5d9cfc40c27911b2ba2db16
next-shiyon <4.man.ach@gmail.com>
2023-11-05 15:42:49 +0900

    🎨 プロフィールを作成

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.scss";
+import utilStyles from "../styles/utils.module.scss";
+import Link from "next/link";
 
-const name = '[Your Name]';
-export const siteTitle = 'Next.js Sample Website';
+const name = "SIYEON PARK";
+export const siteTitle = "TOKYO PARK";
 
 export default function Layout({ children, home }) {
   return (
@@ -14,12 +14,12 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="韓国人フロンドエンドエンジニアのつぶやきブログ"
         />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -37,6 +37,7 @@ export default function Layout({ children, home }) {
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <p>I love Front-End so much ... ✨</p>
           </>
         ) : (
           <>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import utilStyles from "../styles/utils.module.scss";
 
 export default function Home() {
   return (
@@ -8,13 +8,7 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      <section className={utilStyles.headingMd}></section>
     </Layout>
   );
 }
